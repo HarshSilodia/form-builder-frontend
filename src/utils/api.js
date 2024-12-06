fe89cfc6678cb00c4461c9eca86c74f6c41d375a
HarshSilodia <harshsilodia@gmail.com>
2024-12-07 00:11:57 +0530

    updated server url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'https://form-builder-backend-ve0n.onrender.com',
 });
 
 // Save form data
